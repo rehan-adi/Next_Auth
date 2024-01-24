f32be9347ac58e514b08485568ace17236b1e7ab
Rehan <rehanalire52@gmail.com>
2024-01-24 15:43:45 +0530

    Update

--- src/app/Projects/page.jsx
+++ src/app/Projects/page.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function page() {
     return (
-        <div className='h-[100vh] w-auto bg-blue-950'>
-            <div className='flex justify-center pt-40 items-center gap-20'>
-                <div className='bg-[#ebc7b5] flex justify-start pt-10 flex-col items-center rounded-3xl h-[50vh] w-[20vw]'>
+        <div className='h-[210vh] w-auto bg-blue-950'>
+            <div className='flex justify-center pt-32 flex-col items-center gap-20'>
+                <div className='bg-[#ebc7b5] flex justify-center pt-10 flex-col items-center rounded-3xl h-[50vh] w-[55vw]'>
                       <h1>DESIGINE</h1>
-                      <p className='text-2xl mt-7'>How to make a quality <br /> site in WordPress in 40<br /> hours without experience</p>
-                      <a href="" className='mt-20 text-green-600 text-xl font-normal'>Read More</a>
+                      <p className='text-xl ml-4 mt-7'>How to make a qual<br />ity site in WordPress <br />in 40 hours without experience</p>
+                      <a href="" className='mt-16 text-green-600 text-xl font-normal'>Read More</a>
                 </div>
-                <div className='bg-[#ebc7b5] flex justify-start pt-10 flex-col items-center rounded-3xl h-[50vh] w-[20vw]'>
+                <div className='bg-[#ebc7b5] flex justify-center pt-10 flex-col items-center rounded-3xl h-[50vh] w-[55vw]'>
                       <h1>PROGRAMMING</h1>
-                      <p className='text-2xl mt-7'>How to make a quality <br /> site in WordPress in 40<br /> hours without experience</p>
-                      <a href="" className='mt-20 text-green-600 text-xl font-normal'>Read More</a>
+                      <p className='text-xl ml-4 mt-7'>How to make a qual<br />ity site in WordPress <br />in 40 hours without experience</p>
+                      <a href="" className='mt-16 text-green-600 text-xl font-normal'>Read More</a>
                 </div>
-                <div className='bg-[#ebc7b5] flex justify-start pt-10 flex-col items-center rounded-3xl h-[50vh] w-[20vw]'>
+                <div className='bg-[#ebc7b5] flex justify-center pt-10 flex-col items-center rounded-3xl h-[50vh] w-[55vw]'>
                       <h1>SEO</h1>
-                      <p className='text-2xl mt-7'>How to make a quality <br /> site in WordPress in 40<br /> hours without experience</p>
-                      <a href="" className='mt-20 text-green-600 text-xl font-normal'>Read More</a>
+                      <p className='text-xl ml-4 mt-7'>How to make a qual<br />ity site in WordPress <br />in 40 hours without experience</p>
+                      <a href="" className='mt-16 text-green-600 text-xl font-normal'>Read More</a>
                 </div>
             </div>
         </div>
--- src/app/Service/page.jsx
+++ src/app/Service/page.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='bg-blue-950 flex justify-start items-center px-36 gap-20 text-white h-[100vh] w-auto'>
-         <div className='mt-12'>
-             <h1 className='text-4xl'>Professional courseson <br /> creating websites</h1>
-             <p className='mt-8 mb-20'>Our courses are complete professional courses of high quality that include a <br /> set of video  lessons, practical and training materials.</p>
+    <div className='bg-blue-950 flex flex-col justify-center items-center gap-20 text-white min-h-screen w-auto'>
+         <div className='pt-16'>
+             <h1 className='text-xl'>Professional courseson  creating <br /> websites</h1>
+             <p className='mt-8 '>Our courses are complete professional cour<br />ses of high quality that include a set of video <br /> lessons, practical and training materials.</p>
          </div>
-         <div className='mb-16'>
-            <img src="https://assets.website-files.com/61e57244c283e5456130c457/61e945a0392884655da8bf78_step_illustr.svg" width={555} alt="" />
+         <div className='mb-5'>
+            <img src="https://assets.website-files.com/61e57244c283e5456130c457/61e945a0392884655da8bf78_step_illustr.svg" width={200} alt="" />
          </div>
     </div>
   )
