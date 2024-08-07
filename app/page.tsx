4f5e27b71ce3d41dfba6e40b77e07b6c65e8ccc5
Rehan <rehanalire52@gmail.com>
2024-08-07 20:39:17 +0530

    use axios for get data from api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
 
-export default function Home() {
+export default async function Home() {
+  const userData = await getUserDetails();
+
   return (
-    <div>
-       hi
+    <div className="flex flex-col bg-[#000924] text-white justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
