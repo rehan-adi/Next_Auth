706b1062db172c5974a94fde8b8976ef79e9c252
Rehan <rehanalire52@gmail.com>
2024-08-09 13:50:38 +0530

    add zod validation for signin route

--- validations/auth.validation.ts
+++ validations/auth.validation.ts
@@ -5,3 +5,8 @@ export const signupValidation = z.object({
     email: z.string().email(),
     password: z.string().min(6).max(50)
 })
+
+export const signinValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(50)
+})
