2074f9c24a589d42fa007de0780112257c2b6054
Rehan <rehanalire52@gmail.com>
2024-08-18 19:19:43 +0530

    Add link to Sign In page in the signup form

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -148,7 +148,7 @@ const Signup = () => {
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Already have a account?{' '}
                         <a
-                            href="#"
+                            href="/signin"
                             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                         >
                             Sign In to continue
