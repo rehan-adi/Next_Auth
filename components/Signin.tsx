f9c6e0fbc44585e135c0c917ce654321d97f6241
Rehan <rehanalire52@gmail.com>
2024-08-19 16:09:49 +0530

    Update Signin button to reflect submission state

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -121,7 +121,7 @@ const Signin = () => {
                             <button
                                 type="submit"
                                 disabled={isSubmitting}
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className={`flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''}`}
                             >
                                 {isSubmitting ? 'Signing in...' : 'Sign in'}
                             </button>
