f153f4b9762176299f7ae5b7bb78f798263c5f7c
Rehan <rehanalire52@gmail.com>
2024-08-20 23:03:15 +0530

    add navbar in code base

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
 async function getUserDetails() {
+
     const response = await axios.get(
         'https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details'
     );
     return response.data;
+
 }
 
 export default async function Home() {
     const userData = await getUserDetails();
 
     return (
-        <div className="flex flex-col bg-[#000924] text-white justify-center h-screen">
-            <div className="flex justify-center">
+        <div className="bg-[#000924] text-white">
+            <nav className='border-b flex fixed top-0 justify-between items-center border-white border-opacity-10 w-full lg:px-20 px-6 h-20'>
+                 <h1>Logo</h1>
+                 <button className='bg-blue-600 rounded-full px-3 lg:px-5 py-2'>Logout</button>
+            </nav>
+            <div className="flex justify-center bg-[#000924] h-screen items-center">
                 <div className="border p-8 rounded">
                     <div>Name: {userData?.name}</div>
 
