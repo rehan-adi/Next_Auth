6d98c4f9dbbca380d40bc2733e524324961401c9
Rehan <rehanalire52@gmail.com>
2024-08-20 20:36:42 +0530

    change the verificationUrl route

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -39,7 +39,7 @@ export const POST = async (req: NextRequest) => {
             }
         });
 
-        const verificationUrl = `http://localhost:3000/api/user/verify-email?token=${verificationToken}`;
+        const verificationUrl = `http://localhost:3000/verify-email?token=${verificationToken}`;
 
         await resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
