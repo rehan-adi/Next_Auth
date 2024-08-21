d238312ade857df02859f331227d376b265f560e
Rehan <rehanalire52@gmail.com>
2024-08-21 16:02:41 +0530

    remove logo image from Signin page

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -51,11 +51,6 @@ const Signin = () => {
         <>
             <div className="flex min-h-screen bg-[#000924] flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
                         Sign In to your account
                     </h2>
